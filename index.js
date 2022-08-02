d93650aaa6f0bee9c741ff11d57089592de1c6b3
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-08-03 00:39:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, gift){
+
+     return [`Thank you, ${names[0]}, for the wonderful ${gift} gift!`,
+     `Thank you, ${names[1]}, for the wonderful ${gift} gift!`,
+     `Thank you, ${names[2]}, for the wonderful ${gift} gift!`];
+
+};
+
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        number -= 1;
+    }
+  }
